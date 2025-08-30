8a007144ab737927d97d4b3ef322b5c30d60a0bb
Kannabiran <killerkanna2019@outlook.com>
2025-08-30 23:24:56 +0530

    API changes and new user changes

--- InventorySystem.Model/AccountModel.cs
+++ InventorySystem.Model/AccountModel.cs
@@ -1,4 +1,6 @@
-﻿namespace InventorySystem.Model
+﻿using System.Text.Json.Serialization;
+
+namespace InventorySystem.Model
 {
     public class AccountModel
     {
@@ -10,14 +12,14 @@
 
         public class UserModel
         {
-            public int USERID { get; set; }
-            public int USERROLEID { get; set; }
+            //public int USERID { get; set; }
+           // public int USERROLEID { get; set; }
             public string USERNAME { get; set; }
             public string PASSWORDHASH { get; set; }
             public string EMAIL { get; set; }
             public int ISACTIVE { get; set; }
-            public DateTime CREATEDAT { get; set; }
-            public DateTime UPDATEDAT { get; set; }
+            //public DateTime CREATEDAT { get; set; }
+            //public DateTime UPDATEDAT { get; set; }
             public string MOBILE_NUMBER { get; set; }
             public string AADHAR_NUMBER { get; set; }
             public string ADDRESS { get; set; }
